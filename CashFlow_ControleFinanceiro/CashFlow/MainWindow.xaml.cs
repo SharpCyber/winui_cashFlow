bad604b918ada46ed55437620e2d12812cc6e05b
jeyjunior <jeyjuniordg@gmail.com>
2025-08-02 18:38:09 -0300

    Add menu de navegação e configuração inicial de MainWindow

--- CashFlow_ControleFinanceiro/CashFlow/MainWindow.xaml.cs
+++ CashFlow_ControleFinanceiro/CashFlow/MainWindow.xaml.cs
@@ -1,34 +1,96 @@
 using CashFlow.Application;
+using CashFlow.Domain.Enumeration;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using WinRT.Interop;
 
 namespace CashFlow
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        #region Propriedades
+        private const int Largura = 600;
+        private const int Altura = 700;
+        private AppWindow m_AppWindow;
+        private Type paginaAtiva;
+        #endregion
+
+        #region Construtor
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            DefinirPadraoUI();
 
             Configuracao.Iniciar();
         }
+        #endregion
+
+        #region Eventos
+        private async void MainWindow_Closed(object sender, WindowEventArgs args)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+        #endregion
+
+        #region Metodos
+        private void DefinirPadraoUI()
+        {
+            m_AppWindow = ObterAppWindowAtual();
+            m_AppWindow.Title = "FlowCheck";
+            m_AppWindow.SetIcon("Assets/flowcheck_icone_24.ico");
+
+            DefinirTamanhoUI();
+            CentralizarUI();
+
+            var appWindow = ObterAppWindowAtual();
+            if (appWindow != null && Microsoft.UI.Windowing.AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = appWindow.TitleBar;
+
+                titleBar.BackgroundColor = Windows.UI.Color.FromArgb(255, 18, 18, 18);
+                titleBar.ForegroundColor = Colors.White;
+
+                titleBar.InactiveBackgroundColor = titleBar.BackgroundColor;
+                titleBar.InactiveForegroundColor = titleBar.ForegroundColor;
+
+                titleBar.ButtonBackgroundColor = titleBar.BackgroundColor;
+                titleBar.ButtonForegroundColor = titleBar.ForegroundColor;
+                titleBar.ButtonHoverBackgroundColor = Windows.UI.Color.FromArgb(255, 18, 18, 18);
+                titleBar.ButtonHoverForegroundColor = titleBar.ForegroundColor;
+                titleBar.ButtonPressedBackgroundColor = Windows.UI.Color.FromArgb(255, 18, 18, 18);
+            }
+        }
+        private void DefinirTamanhoUI()
+        {
+            m_AppWindow.Resize(new Windows.Graphics.SizeInt32(Largura, Altura));
+        }
+        private void CentralizarUI()
+        {
+            var displayArea = DisplayArea.GetFromWindowId(m_AppWindow.Id, DisplayAreaFallback.Primary);
+            var centralizacao = new Windows.Graphics.PointInt32
+            {
+                X = displayArea.WorkArea.X + (displayArea.WorkArea.Width - Largura) / 2,
+                Y = displayArea.WorkArea.Y + (displayArea.WorkArea.Height - Altura) / 2
+            };
+            m_AppWindow.Move(centralizacao);
+        }
+        private AppWindow ObterAppWindowAtual()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+        #endregion
     }
 }
