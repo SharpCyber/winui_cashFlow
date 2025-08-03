3f6d4310d49ca121ec4b1e97cdfcd5a03074a83b
jeyjunior <jeyjuniordg@gmail.com>
2025-08-03 17:10:23 -0300

    Teste

---
+++ CashFlow_ControleFinanceiro/CashFlow.Application/ConfiguracaoServicos.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Data;
+using CashFlow.Domain.Enumeration;
+using CashFlow.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Application
+{
+    public static class ConfiguracaoServicos
+    {
+        public static void Iniciar()
+        {
+            RegistrarEntidades();
+        }
+        public static void RegistrarEntidades()
+        {
+            var uow = Bootstrap.ServiceProvider.GetRequiredService<IUnitOfWork>();
+        }
+    }
+}
