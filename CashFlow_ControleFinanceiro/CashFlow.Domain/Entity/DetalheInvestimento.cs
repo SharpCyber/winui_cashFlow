535c05d0e4e2f2d70e4a9cd93bc68aeae81b0424
THAYNÁ <THAYN@THAYNA>
2025-08-10 17:47:10 -0300

    Criação das Entidades, Atributos e Ajuste no Dapper

---
+++ CashFlow_ControleFinanceiro/CashFlow.Domain/Entity/DetalheInvestimento.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Domain.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Domain.Entity
+{
+    [Entidade("DetalheInvestimento")]
+    public class DetalheInvestimento
+    {
+        [ChavePrimaria]
+        public int PK_DetalheInvestimento { get; set; }
+
+        [Unico, Obrigatorio, Relacionamento("Transacao", "PK_Transacao")]
+        public int FK_Transacao { get; set; }
+
+        [Obrigatorio, Relacionamento("AtivoFinanceiro", "PK_AtivoFinanceiro")]
+        public int FK_Ativo { get; set; }
+
+        [Obrigatorio]
+        public decimal QuantidadeCotas { get; set; }
+
+        [Obrigatorio]
+        public decimal ValorUnitario { get; set; }
+
+        public decimal? Imposto { get; set; } = 0;
+
+        public decimal? OutrosCustos { get; set; } = 0;
+    }
+}
---
+++ CashFlow_ControleFinanceiro/CashFlow.Domain/Entity/Transacao.cs
@@ -0,0 +1,35 @@
+﻿using CashFlow.Domain.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Domain.Entity
+{
+    [Entidade("Transacao")]
+    public class Transacao
+    {
+        [ChavePrimaria]
+        public int PK_Transacao { get; set; }
+
+        [Obrigatorio, Relacionamento("TipoTransacao", "PK_TipoTransacao")]
+        public int FK_TipoTransacao { get; set; }
+
+        [Obrigatorio, Relacionamento("EntidadeFinanceira", "PK_EntidadeFinanceira")]
+        public int FK_EntidadeFinanceira { get; set; }
+
+        [Obrigatorio]
+        public decimal Valor { get; set; }
+
+        [Obrigatorio]
+        public DateTime DataTransacao { get; set; } = DateTime.Now;
+
+        public DateTime? DataVencimento { get; set; }
+
+        public string Observacao { get; set; }
+
+        [Obrigatorio, Relacionamento("Usuario", "PK_Usuario")]
+        public int FK_Usuario { get; set; }
+    }
+}
