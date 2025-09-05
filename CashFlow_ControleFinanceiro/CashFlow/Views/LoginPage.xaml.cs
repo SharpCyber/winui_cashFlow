17cb7cfbb7ebb9333dbf595a559d346de653f1d7
jeyjunior <jeyjuniordg@gmail.com>
2025-09-04 21:09:31 -0300

    Add icone google 24x24. Add padrao de cor vermelho. Add componentes e estilos para tela login

--- CashFlow_ControleFinanceiro/CashFlow/Views/LoginPage.xaml.cs
+++ CashFlow_ControleFinanceiro/CashFlow/Views/LoginPage.xaml.cs
@@ -1,5 +1,8 @@
-using CashFlow.Domain.Interfaces.ViewModels;
-using CashFlow.ViewModel.MainWindow;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -7,31 +10,35 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation;
+using CashFlow.Domain.Interfaces.ViewModels;
+using CashFlow.ViewModel.MainWindow;
 
 namespace CashFlow.Views
 {
     public sealed partial class LoginPage : Page
     {
+        #region Interfaces
         private IMainWindowViewModel _mainWindowViewModel;
+        #endregion
 
+        #region Propriedades
+        #endregion
+
+        #region Construtor
         public LoginPage()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region Eventos
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             _mainWindowViewModel.ExibirMenuNavegacao = Visibility.Visible;
             (_mainWindowViewModel as MainWindowViewModel)?.NavegarPara(Domain.Enumeration.eTela.TransacaoPage);
         }
-
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
@@ -41,15 +48,25 @@ namespace CashFlow.Views
                 _mainWindowViewModel = viewModel;
             }
         }
-
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void btnLoginGoogle_Click(object sender, RoutedEventArgs e)
         {
 
         }
+        private void btnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void hlbEsqueciMinhaSenha_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void hlbCadastrarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Metodos
+        #endregion
     }
 }
